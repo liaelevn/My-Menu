abbdbdab10349275843a96ac9dd6627b003f42f0
liaelevn <112964705+liaelevn@users.noreply.github.com>
2023-03-03 12:45:54 +0000

    Add files via upload

---
+++ My menu 3.py
@@ -0,0 +1,24 @@
+# Let's start a coffee shop together!! We're going to build a coffee shop using some new Python programming concepts!!
+
+# Let's build robot Barista!!
+
+# print("Hello, Welcome to Cafe Du Lia")
+
+# name = input(" What is your name? \n ")
+
+# print(" Hello ...... , thank you for coming in today !! ")
+# print("Hello , " + name + " , thank you so much for coming today. ")
+
+# print(" Hello, Welcome to Cafe Du Lia!!! ")
+# name = input(" What is your name? ")
+
+print(" Hello! Welcome to Cafe Du Lia! ")
+name = input("What is your name?\n")
+
+menu = " Hot Chocolate, Lotus Biscuit Tea, Oreo Milk Tea "
+print(name + " what would you like from our menu? We have " + menu)
+
+order = input()
+
+print("I'll have " + menu + "please ")
+print(menu + " coming right up ! ")
